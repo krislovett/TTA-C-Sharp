0c7d6315815bcb35aebd40cdf3d2095f2a33e7b8
klovett84 <84836870+klovett84@users.noreply.github.com>
2021-08-13 12:29:34 -0500

    Added Strings project

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            //concatenate three strings into one and convert it to uppercase
+            string string1 = "A ";
+            string string2 = "concatenated ";
+            string string3 = "string.";
+            string bigString = (string1 + string2 + string3).ToUpper();
+            Console.WriteLine(bigString);
+
+            //create a stringbuilder and append new sentences to make a paragraph.
+            StringBuilder sb = new StringBuilder("Lorem ipsum dolor sit amet, consectetur adipiscing elit. ");
+            sb.Append("Vestibulum rhoncus arcu et sodales auctor. ");
+            sb.Append("Vivamus eget nisi ipsum.");
+            Console.WriteLine(sb);
+
+            Console.Read();
+        }
+    }
+}
