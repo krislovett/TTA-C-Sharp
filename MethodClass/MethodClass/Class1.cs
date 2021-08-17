520821b4316e3934909d8cacd38c462b801cd9df
klovett84 <84836870+klovett84@users.noreply.github.com>
2021-08-17 14:56:39 -0500

    Added MethodClass project

---
+++ MethodClass/MethodClass/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Class1
+    {
+        public Class1()
+        {
+        }
+
+        //create void method that takes two ints as parameters. Add 2 to Num1 and print Num2 to console.
+        public static void IntMethod(int Num1, int Num2)
+        {
+            int addResult = Num1 + 2;
+            Console.WriteLine(Num2);
+        }
+    }
+}
---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 Class1 = new Class1();
+
+            //call IntMethod, passing in two ints
+            Class1.IntMethod(5, 10);
+
+            //call IntMethod, passing in two ints by name
+            Class1.IntMethod(Num1: 12, Num2: 13);
+
+            Console.Read();
+        }
+    }
+}
