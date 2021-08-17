23c23464aff195fc7d48b12b095124e21051932a
klovett84 <84836870+klovett84@users.noreply.github.com>
2021-08-17 01:43:38 -0500

    Added CallingMethods project and TwentyOne project

---
+++ CallingMethods/CallingMethods/Numbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Numbers
+    {
+        //create constructor for Numbers object, setting values for AddNum, SubtractNum, and MultiplyNum
+        public Numbers()
+        {
+            AddNum = 12;
+            SubtractNum = 4;
+            MultiplyNum = 10;
+        }
+
+        //define properties for Numbers class
+        public int AddNum { get; set; }
+        public int SubtractNum { get; set; }
+        public int MultiplyNum { get; set; }
+
+        //define methods to perform various math operations with AddNum, SubtractNum, MultiplyNum, and user's input, then return results
+        public int AddNumbers(int inputNum)
+        {
+            int addResult = inputNum + AddNum;
+            return addResult;
+        }
+
+        public int SubtractNumbers(int inputNum)
+        {
+            int subtractResult = inputNum - SubtractNum;
+            return subtractResult;
+        }
+
+        public int MultiplyNumbers(int inputNum)
+        {
+            int multiplyResult = inputNum * MultiplyNum;
+            return multiplyResult;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create new numbers object
+            Numbers numbers = new Numbers();
+            Console.WriteLine("Please enter a whole number:");
+            int inputNum = Convert.ToInt32(Console.ReadLine());
+
+            //call on each Numbers method and print results
+            Console.WriteLine(inputNum + " + " + numbers.AddNum + " = " +  numbers.AddNumbers(inputNum) + "\n");
+            Console.WriteLine(inputNum + " - " + numbers.SubtractNum + " = " + numbers.SubtractNumbers(inputNum) + "\n");
+            Console.WriteLine(inputNum + " * " + numbers.MultiplyNum + " = " + numbers.MultiplyNumbers(inputNum) + "\n");
+            Console.Read();
+        }
+    }
+}
