e8f6734ce599382b9758595afe29ccdbda0e4056
klovett84 <84836870+klovett84@users.noreply.github.com>
2021-08-17 13:25:23 -0500

    Added MainMethod project and updated TwentyOne

---
+++ MainMethod/MainMethod/Numbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Numbers
+    {
+        public Numbers()
+        {
+            AddNum = 4;
+            DivideNum = 1.5m;
+            StringNum = 12;
+        }
+
+        public int AddNum { get; set; }
+        public decimal DivideNum { get; set; }
+        public int StringNum { get; set; }
+
+        //DoMath adds two numbers together when int is passed in
+        public int DoMath(int AddNum2)
+        {
+            int addResult = AddNum + AddNum2;
+            return addResult;
+        }
+
+        //DoMath divides number by input number when decimal is passed in
+        public decimal DoMath(decimal DivideNum2)
+        {
+            decimal divideResult = DivideNum / DivideNum2;
+            return divideResult;
+        }
+
+        //DoMath converts input string to int and subtracts StringNum from it
+        public int DoMath(string StringNum2)
+        {
+            int subtractResult = StringNum - Convert.ToInt32(StringNum2);
+            return subtractResult;
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create new numbers object
+            Numbers numbers = new Numbers();
+
+            //call on DoMath method and pass in int to return addResult
+            Console.WriteLine("Please enter a whole number:");
+            Console.WriteLine(numbers.DoMath(Convert.ToInt32(Console.ReadLine())));
+
+            //call on DoMath method and pass in decimal to return divideResult
+            Console.WriteLine("Please enter a decimal:");
+            Console.WriteLine(numbers.DoMath(Convert.ToDecimal(Console.ReadLine())));
+
+            //call on DoMath method and pass in string to return subtractResult
+            Console.WriteLine("Please enter a whole number:");
+            Console.WriteLine(numbers.DoMath(Console.ReadLine()));
+
+            Console.Read();
+        }
+    }
+}
