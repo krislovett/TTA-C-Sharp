84c0197a1a40fa2e56259663a83bfefb259c32c0
klovett84 <84836870+klovett84@users.noreply.github.com>
2021-08-17 13:46:08 -0500

    Added OptionalParameters project

---
+++ OptionalParameters/OptionalParameters/Numbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    class Numbers
+    {
+        public Numbers()
+        {
+            AddNum = 14;
+        }
+
+        public int AddNum { get; set; }
+
+        //AddNumbers method adds user input numbers with AddNum. AddNum3 is optional.
+        public int AddNumbers(int AddNum2, int AddNum3 = 0)
+        {
+            int addResult = AddNum + AddNum2 + AddNum3;
+            return addResult;
+        }        
+    }
+}
---
+++ OptionalParameters/OptionalParameters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers numbers = new Numbers();
+            Console.WriteLine("Enter a whole number.");
+            int AddNum2 = Convert.ToInt32(Console.ReadLine());
+
+            //collect second optional number from user and save as string
+            Console.WriteLine("Enter another whole number (optional).");
+            string AddNum3string = Console.ReadLine();
+            
+            //if AddNum3string is not empty, convert to int and pass into AddNumbers method
+            if (AddNum3string != "")
+            {
+                int AddNum3 = Convert.ToInt32(AddNum3string);
+                Console.WriteLine("{0} + {1} + {2} = " + numbers.AddNumbers(AddNum2, AddNum3), numbers.AddNum, AddNum2, AddNum3);
+            }
+
+            //if AddNum3string is empty, ignore and pass just AddNum2 into AddNumbers method
+            if (AddNum3string == "")
+            {
+                Console.WriteLine("{0} + {1} = " + numbers.AddNumbers(AddNum2), numbers.AddNum, AddNum2);
+            }
+
+            Console.Read();
+        }
+    }
+}
