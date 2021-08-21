e435dcf490e87046ad62a134b30b9effb76fd1f6
klovett84 <84836870+klovett84@users.noreply.github.com>
2021-08-20 19:56:34 -0500

    Added DaysOfTheWeek project and updated TwentyOne

---
+++ DaysOfTheWeek/DaysOfTheWeek/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfTheWeek
+{
+    class Program
+    {
+        static void Main()
+        {
+            //ask user to enter today's day and assign to variable
+            Console.WriteLine("Enter today's current day of the week:");
+            string dayInput = Console.ReadLine().ToLower();
+
+            try
+            {
+                //parse through user's input to find that value in the enum, then assign it to currentDay variable
+                Days currentDay = (Days)Enum.Parse(typeof(Days), dayInput);
+                Console.WriteLine("Success!");
+            }
+            catch (Exception)
+            {
+                //print error message if user inputs anything other than a day of the week
+                Console.WriteLine("Please enter an acutal day of the week.");
+            }
+
+            Console.Read();
+        }
+
+        //create enum of days of the week
+        public enum Days { monday, tuesday, wednesday, thursday, friday, saturday, sunday };
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,13 +10,6 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Game game = new TwentyOneGame();
-            game.Players = new List<Player>();
-            Player player = new Player();
-            player.Name = "Mr B";
-            game += player;
-            game -= player;
-
             Deck deck = new Deck();
             deck.Shuffle(3);
 
@@ -25,7 +18,6 @@ namespace TwentyOne
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
 
-            Console.WriteLine(deck.Cards.Count);
             Console.Read();
         }
     }
