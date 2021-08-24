0c3e5e3e156a5847f57b4e9f89bbdb86b7e13223
klovett84 <84836870+klovett84@users.noreply.github.com>
2021-08-24 16:40:52 -0500

    Added Time project and updated TwentyOne

---
+++ Time/Time/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //print current date and time to console
+            Console.WriteLine("Current time: " + DateTime.Now);
+
+            //ask user to input a number and save to num
+            Console.WriteLine("Enter a number.");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            //add num hours to current time and print new time
+            DateTime newTime = DateTime.Now.AddHours(num);
+            Console.WriteLine("In {0} hours, it will be: {1}", num, newTime);
+
+
+            Console.Read();
+        }
+    }
+}
