a53f6cd9f526039d41117e50b946896e57fde953
klovett84 <84836870+klovett84@users.noreply.github.com>
2021-08-30 14:57:06 -0500

    Added CarInsurance and AgeCheck projects and updated TwentyOne

---
+++ AgeCheck/AgeCheck/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age:");
+
+            //try to take user input for age and set to int, then print to console.
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+                
+                //if age is a negative number, throw error
+                if (age < 0)
+                {
+                    throw new ArgumentException();
+                }
+                Console.WriteLine("Your age is: {0}", age);
+                Console.Read();
+            }
+
+            //catch exception for negative age and print error message
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Age cannot be negative.");
+                Console.Read();
+                return;
+            }
+
+            //catch any other exceptions and print error message
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.Read();
+                return;
+            }
+        }
+    }
+}
